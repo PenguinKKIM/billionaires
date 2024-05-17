e7d00daa49b61f0cd5a6b7dfe604c1126dce1be2
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-05-18 00:37:50 +0900

    fix : title 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,19 @@ import "../app/styles/reset.css";
 import "../app/styles/global.css";
 import styles from "../app/styles/Layout.module.css";
 import { Metadata } from "next";
-// import Navigation from "../components/navigation";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | 억만장자들",
-    default: "억만장자들",
+    template: "%s | Billionaire",
+    default: "Billionaire",
   },
-  description: "억만장자들을 간단히 소개해 드립니다.",
+  description: "Introduce Billionaire.",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        {/* <Navigation /> */}
         <div className={styles.allcontainer}>{children}</div>
       </body>
     </html>
